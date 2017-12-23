95263d310da34605ea7176c8de261601e75bf480
way11229 <way11229@gmail.com>
2017-12-23 10:25:43 +0000

    Way

--- application/views/layout/layoutbody.php
+++ application/views/layout/layoutbody.php
@@ -1,3 +1,12 @@
 <header>
-  
+  <div class="leftArea">  
+    <p>Selldarity</p>
+  </div>
+  <div class="rightArea">
+    <p>註冊</p>
+    <button>登入</button>
+    <img src="<?=$baseUrl?>/css/images/notification.svg" class="notification"/>
+    <img src="<?=$baseUrl?>/css/images/question.svg" class="question"/>
+    <img src="<?=$baseUrl?>/css/images/search.svg" class="search"/>
+  </div>
 </header>
