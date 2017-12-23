00803a1d414f88ec2b6d8a8b3c11ebdfd8b1c68c
frankyu123 <frank85515@gmail.com>
2017-12-23 11:38:54 +0000

    frankyu test layoutheader

--- application/views/layout/layoutbody.php
+++ application/views/layout/layoutbody.php
@@ -1,14 +1,18 @@
 <header>
   <div class="pure-g">
-    <div class="leftArea pure-u-1-4">  
+    <div class="leftHeader pure-u-1-4">  
       <img src="<?=$baseUrl?>/css/images/Selldarity_icon_chinese.svg" class="logo"/>
     </div>
-    <div class="rightArea pure-u-3-4">
-      <p>註冊</p>
-      <button>登入</button>
-      <img src="<?=$baseUrl?>/css/images/notification.svg" class="notification"/>
-      <img src="<?=$baseUrl?>/css/images/question.svg" class="question"/>
-      <img src="<?=$baseUrl?>/css/images/search.svg" class="search"/>
+    <div class="rightHeader pure-u-3-4">
+      <div class="leftHeaderInfo">
+        <p>註冊</p>
+        <button>登入</button>
+      </div>
+      <div class="rightHeaderInfo">
+        <img src="<?=$baseUrl?>/css/images/notification.svg" class="notification"/>
+        <img src="<?=$baseUrl?>/css/images/question.svg" class="question"/>
+        <img src="<?=$baseUrl?>/css/images/search.svg" class="search"/>
+      </div>
     </div>
   </div>
 </header>
